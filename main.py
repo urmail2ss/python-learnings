f26668d69e3816f410a8cd581d4c654a193ab130
Shanmuga Sundaram Natarajan <urmail2ss@gmail.com>
2020-01-22 18:53:42 +0000

    Operator Precedence https://docs.python.org/3/reference/expressions.html#operator-precedence

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-## Data Types
-print (type(4))
-print (type(4/3))
-print (type(4.3))
-print (type(4 * 3))
-print ((4 ** 3))
-print ((4 // 3))
-print ((4 % 3))
+#Operator Precedence
 
+#https://docs.python.org/3/reference/expressions.html#operator-precedence
+print((5 + 4) * 10 / 2)
+
+print(((5 + 4) * 10) / 2)
+
+print((5 + 4) * (10 / 2))
+
+print(5 + (4 * 10) / 2)
+
+print(5 + 4 * 10 // 2)
